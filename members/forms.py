42346902956f57499684812a58602d67553ff601
Fariha Rahman Saba <sabarahman.official@gmail.com>
2023-03-26 21:35:06 +0600

    last commit 2

--- members/forms.py
+++ members/forms.py
@@ -23,7 +23,7 @@ class SignUpForm(UserCreationForm):
 	email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
 	first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 	last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-
+	
 	class Meta:
 		model = User
 		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2' )
